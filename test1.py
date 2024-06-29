87098784b04ba5e773bcf30492763df285d742c7
hamedHT <78105470+hamedHT@users.noreply.github.com>
2024-06-29 18:09:41 +0330

    جلسه1

---
+++ test1.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("I'm hamed from rad parsina")
+print('I am hamed from "radparsina"')
+print(''' I'm hamed from "rad parsina"''')
+print(""" hamed
+      radparsina
+      python""")

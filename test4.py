f1f8b2dd7d026e6c564cc0ada828c9c29e39df40
hamedHT <78105470+hamedHT@users.noreply.github.com>
2024-07-17 16:37:32 +0330

    جلسه چهارم

---
+++ test4.py
@@ -0,0 +1,37 @@
+num1 = num2 = 50
+num1 , num2 = 50,100
+num1 , plus , num2 = 10 , '+' , 100
+
+word = "python"
+char = word[0]
+char = word[-1]
+
+#string slicing
+
+message = "Hello world!"
+slice1 = message[6:11]
+slice2 = message[:5]
+slice3 = message[6:]
+slice4 = message[::-1]
+print(len(message))
+
+my_list = [10 , "iran" , 1.2]
+my_list2 = [10 , 20 , 30]
+List = my_list + my_list2
+List1 = [my_list2  , 1]
+List[2] = 2.5
+print(List)
+
+import random
+
+taas = random.randint(1,6)
+print(taas)
+
+
+#تمرین در منزل 
+#یک شماره کارت از کاربر دریافت کرده و به جز چهار رقم اخر مابقی ارقام بصورت * باشد
+#رشته معکوس شده زیر را برگردانید
+#imedacA anisraP-daR ni esruoc nohtyP
+#برنامه ای بنویسید که با دریافت نام یک فایل از کاربر پسوند انرا نمایش دهد
+#برنامه ای بنویسید که با دریافت نام کاربر حرف اول و اخر نام او را جابجا کند
+

e96dc8e335f88c5280507b75ca029522bfad22e9
hamedHT <78105470+hamedHT@users.noreply.github.com>
2024-07-31 17:16:38 +0330

    جلسه 8

---
+++ test8.py
@@ -0,0 +1,53 @@
+# vaam = int(input("enter your value: "))
+# aghsat = int(input("enter your number of aghsat: "))
+# if vaam > 100:
+#     if aghsat > 10:
+#         print('bahre shoma hast 20%')
+#         bahre = vaam *20/100
+#         print('bedehi shoma hast : ',vaam+bahre)
+#     else:
+#         print('bahre shoma hast 15%')
+#         bahre = vaam *15/100
+#         print('bedehi shoma hast : ',vaam+bahre)
+# else:
+#     if aghsat > 10:
+#        print('bahre shoma hast 10%')
+#        bahre = vaam *10/100
+#        print('bedehi shoma hast : ',vaam+bahre)
+#     else:
+#         print('bahre shoma hast 5%')
+#         bahre = vaam *5/100
+#         print('bedehi shoma hast : ',vaam+bahre) 
+
+
+
+# numbers = range(0,6,1)
+# print(numbers)
+
+# for i  in range(0,6):
+#     print(i)
+
+# max = 0
+# for i in range(11):
+#     num = int(input('enter a number %d: '%(i)))
+#     if num > max:
+#         max = num
+
+# print('the maximum number is ',max)        
+
+for i in range(10):
+    print('*'*i)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+  
+#فعالیت منزل
+#برنامه ای بنویسید که مجموع اعداد یک تا صد تا نمایش دهد
+#برنامه ای بنویسید که فاکتوریل یک عدد را محاسبه کند
+# برنامه بنویسید که اعداد اول یک تا 100 را نمایش دهد
+# برنامه  بنویسید که یک هرم ستاره به طول 5 نمایش دهد
+# برنامه به بنویسید که اعداد زوج و فرد یک تا صد را در دو لیست جداگانه قرار دهد
+# برنامه بنویسید که یک لیست 10 عددی از کاربر بگیرد و اعداد انرا بطور معکوس چاپ کند   

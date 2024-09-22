586520263edd3a37ae42720e14ab78ad186baaba
hamedHT <78105470+hamedHT@users.noreply.github.com>
2024-09-22 16:56:54 +0330

    جلسه 1

---
+++ AdvancePython/Session1.py
@@ -0,0 +1,28 @@
+# import reizi as hesab
+    
+# my_math = hesab.math(20,10)
+# my_math2 = hesab.math(100,50)
+
+# print(my_math.addition())
+# print(my_math.subtraction())
+
+# print(my_math2.addition())
+
+
+# class student:
+#     def __init__(self, name, family, age):
+#         self.name = name
+#         self.family = family
+#         self.age = age
+
+# student1 = student('Ali','rezaee',22)
+# student2 = student('zahra','akbari',18) 
+# print(student1.__dict__)
+# print(student2.__dict__)       
+
+
+import HamedModule as hamed
+
+
+hesab1 = hamed.machine_hesab(10,20)
+print(hesab1.division())
